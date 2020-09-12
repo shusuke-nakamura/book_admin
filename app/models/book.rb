49453695afe2c346fe58ad2d32496bd86eabd718
Shusuke Nakamura <shuchan.galaxy@gmail.com>
2020-09-12 21:33:09 +0900

    リスト2.11まで学習完了

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,11 @@ class Book < ApplicationRecord
   scope :written_about, -> (them) {where("name like ?", "%#{them}%")}
 
   belongs_to :publisher
+  has_many :book_authors
+  has_many :authors, through: :book_authors
+
+  validates :name, presence: true
+  validates :name, lenth: { maximum: 25 }
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+  
 end
